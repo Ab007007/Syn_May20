bf02e928731d2c90b6c8898d9a85a9dcf8ea9876
Aravinda <aru03.info@gmail.com>
2019-05-21 09:53:18 +0530

    day 2- start

---
+++ src/com/synechron/training/basics/waits/AutoSuggestionsDemo.java
@@ -0,0 +1,56 @@
+package com.synechron.training.basics.waits;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AutoSuggestionsDemo {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("Synechron");
+		
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//ul[@class='erkvQe']"))));
+		
+		List<WebElement> autoSuggestions = driver.findElements(By.xpath("//li[@class='sbct']"));
+		
+		
+		System.out.println(autoSuggestions.size());
+		
+		for(WebElement suggestion:autoSuggestions)
+		{
+			System.out.println(suggestion.getText());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
