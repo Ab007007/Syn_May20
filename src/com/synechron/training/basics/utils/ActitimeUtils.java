08f6a3f5c91f47cfdc8c80f89a225f4c3398b0b0
Aravinda <aru03.info@gmail.com>
2019-05-22 08:43:44 +0530

    day 3- Utils

--- src/com/synechron/training/basics/utils/ActitimeUtils.java
+++ src/com/synechron/training/basics/utils/ActitimeUtils.java
@@ -32,9 +32,9 @@ public class ActitimeUtils extends DriverUtils
 	public  static void loginToActitime(String un, String pwd)
 	{
 		System.out.println("logging into applicaiont with username " + un + " and password " + pwd);
-		driver.findElement(By.id("username")).sendKeys(un);
-		driver.findElement(By.name("pwd")).sendKeys(pwd);
-		driver.findElement(By.id("loginButton")).click();
+		typeOnElement("id", "username", "admin");
+		typeOnElement("name", "pwd", "manager");
+		clickOnElement("id", "loginButton");
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
--- src/com/synechron/training/basics/utils/DriverUtils.java
+++ src/com/synechron/training/basics/utils/DriverUtils.java
@@ -2,25 +2,97 @@ package com.synechron.training.basics.utils;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverUtils {
 
 	public static WebDriver driver = null;
-	
-	
-	public static WebDriver getMyDriver()
-	{
+
+	public static WebDriver getMyDriver() {
 		System.out.println("Creating a webdriver object");
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 		return driver;
-	
+
 	}
+
+	public static WebElement getMyElement(String locator, String locatorValue) {
+		WebElement ele = null;
+		System.out.println("Finding the element with " + locator + " and " + locatorValue);
+
+		switch (locator.toLowerCase()) {
+		case "id":
+			ele = driver.findElement(By.id(locatorValue));
+			break;
+		case "name":
+			ele = driver.findElement(By.name(locatorValue));
+			break;
+		case "classname":
+			ele = driver.findElement(By.className(locatorValue));
+			break;
+		case "tagname":
+			ele = driver.findElement(By.tagName(locatorValue));
+			break;
+		case "linktext":
+			ele = driver.findElement(By.linkText(locatorValue));
+			break;
+		case "partiallinktext":
+			ele = driver.findElement(By.partialLinkText(locatorValue));
+			break;
+		case "css":
+			ele = driver.findElement(By.cssSelector(locatorValue));
+			break;
+		case "xpath":
+			ele = driver.findElement(By.xpath(locatorValue));
+			break;
+
+		default:
+			System.out.println("Please pass the correct locator");
+			break;
+		}
+		
+		return ele;
+
+	}
+
+	
+	public static void typeOnElement(String locator,String locatorValue,String textTotype)
+	{
+		System.out.println("------------Entering text " + textTotype);
+		getMyElement(locator, locatorValue).sendKeys(textTotype);
+		System.out.println("------------Entered Text on element successfully");
+	}
+	
+	
+	public static void clickOnElement(String locator,String locatorValue)
+	{
+		System.out.println("-------------clicking on element " );
+		getMyElement(locator, locatorValue).click();
+		System.out.println("-------------Click operation is successfull");
+	}
+	
+	
+	public static String getTextOnElement(String locator, String locatorValue)
+	{
+		String text = null;
+		text = getMyElement(locator, locatorValue).getText();
+		System.out.println("Text found on the element " + text);
+		return text;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
