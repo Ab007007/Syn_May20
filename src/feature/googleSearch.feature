acb71b27eb15a62792b06595dbd8a720d1491db4
Aravinda <aru03.info@gmail.com>
2019-08-09 13:36:33 +0530

    Cucumber Jars

--- src/feature/googleSearch.feature
+++ src/feature/googleSearch.feature
@@ -1,25 +1,5 @@
-#Author: your.email@your.domain.com
-#Keywords Summary :
-#Feature: List of scenarios.
-#Scenario: Business rule through list of steps with arguments.
-#Given: Some precondition step
-#When: Some key actions
-#Then: To observe outcomes or validation
-#And,But: To enumerate more Given,When,Then steps
-#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
-#Examples: Container for s table
-#Background: List of steps run before each of the scenarios
-#""" (Doc Strings)
-#| (Data Tables)
-#@ (Tags/Labels):To group Scenarios
-#<> (placeholder)
-#""
-## (Comments)
-#Sample Feature Definition Template
 @tag
 Feature: Google Search
-  I want to use this template for my feature file
-
   @tag1
   Scenario: Search A text and print the Search result
     Given I launch google Page
@@ -27,4 +7,12 @@ Feature: Google Search
     Then I should be able to print all links starts with UBS
     And Close the browser
 
+  
+   Scenario: Search A text and print the Search result
+    Given I launch google Page
+    When Search for a text "PUNE"
+    Then I should be able to print all links starts with UBS
+    And Close the browser
+
+  
   
--- src/stepdefinition/LoginTest.java
+++ src/stepdefinition/LoginTest.java
@@ -1,5 +1,61 @@
 package stepdefinition;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
 
+	WebDriver driver = null;
+	@Given("^I launch google Page$")
+	public void i_launch_google_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+	}
+
+	@When("^Search for a text UBS$")
+	public void search_for_a_text_UBS() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("q")).sendKeys("UBS");
+		driver.findElement(By.name("btnK")).click();
+	}
+
+	@Then("^I should be able to print all links starts with UBS$")
+	public void i_should_be_able_to_print_all_links_starts_with_UBS() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		List<WebElement> searchResults = driver.findElements(By.xpath("//h3"));
+		
+		
+		for (WebElement webElement : searchResults) {
+			System.out.println(webElement.getText());
+		}
+	}
+
+	@Then("^Close the browser$")
+	public void close_the_browser() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.close();
+		driver = null;
+	}
+
+	
+	@When("^Search for a text \"([^\"]*)\"$")
+	public void search_for_a_text(String arg1) throws Throwable {
+		driver.findElement(By.name("q")).sendKeys(arg1);
+		driver.findElement(By.name("btnK")).click();
+	}
 }
